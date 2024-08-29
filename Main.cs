a162963826adf95e58f1a50fc8563c6e36d4f2fa
Grab00 <grabcho@outlook.com>
2024-08-29 19:14:56 +0300

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+
+namespace InfectionFixer
+{
+    [BepInPlugin("com.grab.gtag.tagfixer", "TagFixer", "1.0.0")]
+    public class Main : BaseUnityPlugin
+    {
+        public Harmony instance = new Harmony("com.grab.gtag.tagfixer");
+        public void OnEnable()
+        {
+            instance.PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(VRRig), "LateUpdate")]
+    internal class InfectionFixer
+    {
+        private static void Postfix(VRRig __instance)
+        {
+            __instance.mainSkin.material = __instance.materialsToChangeTo[__instance.setMatIndex];
+        }
+    }
+}
